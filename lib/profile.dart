409ac6efbc51213d0b5d6d7b0379c08b117420a0
ysai258 <ysaimuppineni789@gmail.com>
2021-05-16 22:42:17 +0530

    Added Profile and Video

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,11 +1,116 @@
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
+  @override
+  _Profile createState() => _Profile();
+}
+
+class _Profile extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Profile Page'),
-      backgroundColor: Colors.red,
+      body: new SingleChildScrollView(
+          child: Column(children: <Widget>[
+        Container(
+          child: Column(children: <Widget>[
+            Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    width: 3,
+                    color: Colors.orange,
+                  ),
+                ),
+                child: Image.asset(
+                  'assets/images/usericon.png',
+                  height: 120.0,
+                  width: 120.0,
+                ),
+                padding: EdgeInsets.all(1)),
+            Text(
+              'Yashwanth Sai Muppineni',
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white)),
+                child: Text(
+                  'Edit Profile',
+                  style: TextStyle(
+                    color: Colors.orange,
+                  ),
+                ))
+          ]),
+          constraints: BoxConstraints(minHeight: 240),
+          padding: EdgeInsets.all(20.0),
+          color: Colors.grey[200],
+          alignment: Alignment.center,
+        ),
+        Container(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: <Widget>[
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child:
+                      Text('Location', style: TextStyle(color: Colors.grey))),
+              TextField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 0)),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Pincode', style: TextStyle(color: Colors.grey))),
+              TextField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(), hintText: 'xxxxxx'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 0)),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Date of Birth',
+                      style: TextStyle(color: Colors.grey))),
+              TextField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(), hintText: 'dd-mm-yy'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 0)),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Gender', style: TextStyle(color: Colors.grey))),
+              TextField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(), hintText: 'Male'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 0)),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child:
+                      Text('Whatsapp', style: TextStyle(color: Colors.grey))),
+              TextField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: '+91 - xxxxxxxxxx'),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 0)),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Email', style: TextStyle(color: Colors.grey))),
+              TextField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: 'xxxxxxxxxxxx@gmail.com'),
+              ),
+            ],
+          ),
+        )
+      ])),
     );
   }
 }
--- lib/videos.dart
+++ lib/videos.dart
@@ -1,11 +1,183 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Videos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return VideoPlayer();
+  }
+}
+
+class VideoPlayer extends StatefulWidget {
+  @override
+  _VideoPlayerState createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  var titles = [
+    'This is Heading of the realted new ws thi is another',
+    'This is Heading of the realte',
+    'This is Heading of the ralte new ws this is and go on',
+  ];
+  var descp = [
+    'This is Heading of the realted new ws this is another Heading of the',
+    'This is Heading of the realted new ws this is another Heading of the this will go like this when nothing',
+    'This is Heading of the realted new ws this is another Heading of the',
+  ];
+  var img = [
+    'assets/images/img1.jpg',
+    'assets/images/img2.jpg',
+    'assets/images/img3.jpg',
+  ];
+  int topIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Text('Video Page'),
-      backgroundColor: Colors.blue,
-    );
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Videos',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Image.asset(
+              img[topIndex],
+              height: height * 0.25,
+              width: width,
+              fit: BoxFit.fill,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: width * 0.9,
+                  child: Text(
+                    titles[topIndex],
+                    maxLines: 1,
+                    overflow: TextOverflow.fade,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Date & Time',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+                Container(
+                  width: width * 0.9,
+                  child: Text(
+                    descp[topIndex],
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateColor.resolveWith(
+                              (s) => Colors.grey[300])),
+                      child: Center(
+                        child: Text(
+                          'Information',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: img.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        topIndex = index;
+                      });
+                    },
+                    child: Card(
+                      child: Row(
+                        children: <Widget>[
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Image.asset(
+                              img[index],
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  width: width * 0.6,
+                                  child: Text(
+                                    titles[index],
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  width: width * 0.6,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.calendar_today_outlined,
+                                        color: Colors.grey,
+                                      ),
+                                      Text(
+                                        '03-03-2021',
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                      Container(
+                                        width: width * 0.15,
+                                        color: Colors.deepOrange,
+                                        child: Center(
+                                          child: Text(
+                                            'Info',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+          ],
+        ));
   }
 }
